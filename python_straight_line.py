10f3ca185c8ad933701f5c4a3e3f03d6c4e0a3f0
Mark Peters <mapeters101@earthlink.net>
2024-10-22 23:05:09 -0400

    Lesson012:  Complete lesson.

--- python_straight_line.py
+++ python_straight_line.py
@@ -3,6 +3,20 @@ import matplotlib.gridspec as gridspec
 import matplotlib.animation as animation
 import numpy as np
 
+# Set up the duration for your animation
+t0=0 # [hrs]
+t_end=2 # [hrs]
+dt=0.005 # [hrs]
+
+# Create array for time
+t=np.arange(t0,t_end+dt,dt)
+
+# Create an x array
+x=800*t # [km]
+
+# Create a y array
+altitude=2 # [km]
+y=np.ones(len(t))*altitude
 
 
 
