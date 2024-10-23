e241486c06045ce95561e6bb300d4270b75dbe36
Mark Peters <mapeters101@earthlink.net>
2024-10-23 11:17:16 -0400

    Lesson019:  Complete lesson.

--- python_straight_line.py
+++ python_straight_line.py
@@ -26,21 +26,19 @@ def update_plot(num):
     plane_trajectory.set_data(x[0:num],y[0:num])
     plane_trajectory2.set_data(x[0:num],y[0:num])
 
-    return plane_trajectory,plane_trajectory2
+    return plane_trajectory2,plane_trajectory
 
 fig=plt.figure(figsize=(16,9),dpi=120,facecolor=(0.8,0.8,0.8))
 gs=gridspec.GridSpec(2,2)
 gs2=gridspec.GridSpec(3,3)
 
 # Subplot 1
-ax0=fig.add_subplot(gs2[1,1],facecolor=(0.9,0.9,0.9))
-plane_trajectory,=ax0.plot([],[],'g',linewidth=2)
+ax0=fig.add_subplot(gs[1,1],facecolor=(0.9,0.9,0.9))
+plane_trajectory,=ax0.plot([],[],'g',linewidth=20)
 plt.xlim(x[0],x[-1])
 plt.ylim(0,y[0]+1)
 
-
 ax2=fig.add_subplot(gs2[0,2],facecolor=(1,1,1))
-
 plane_trajectory2,=ax2.plot([],[],'r',linewidth=8)
 plt.xlim(x[0],x[-1])
 plt.ylim(0,y[0]+1)
