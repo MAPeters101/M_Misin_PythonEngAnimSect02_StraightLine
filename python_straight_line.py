1f2ac5e50ea65a7709d4f97c51e5acc67122bbab
Mark Peters <mapeters101@earthlink.net>
2024-10-23 00:26:01 -0400

    Lesson016:  Complete lesson.

--- python_straight_line.py
+++ python_straight_line.py
@@ -18,23 +18,24 @@ x=800*t # [km]
 altitude=2 # [km]
 y=np.ones(len(t))*altitude
 
+
 #################### ANIMATION ####################
 frame_amount=len(t)
 
 def update_plot(num):
 
+    plane_trajectory.set_data(x[0:num],y[0:num])
 
-    print(num)
-    print(t[0:num])
-    return
+    return plane_trajectory,
 
 fig=plt.figure(figsize=(16,9),dpi=120,facecolor=(0.8,0.8,0.8))
 gs=gridspec.GridSpec(2,2)
 
 # Subplot 1
 ax0=fig.add_subplot(gs[0,:],facecolor=(0.9,0.9,0.9))
-
-
+plane_trajectory,=ax0.plot([],[],'g',linewidth=2)
+plt.xlim(x[0],x[-1])
+plt.ylim(0,y[0]+1)
 
 plane_ani=animation.FuncAnimation(fig,update_plot,frames=frame_amount,interval=20,repeat=True,blit=True)
 plt.show()
