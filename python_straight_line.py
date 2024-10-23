54e7c792883ae272db2a379a7f30e65f7a917442
Mark Peters <mapeters101@earthlink.net>
2024-10-23 13:22:28 -0400

    Lesson032:  Complete lesson.

--- python_straight_line.py
+++ python_straight_line.py
@@ -24,13 +24,18 @@ frame_amount=len(t)
 
 def update_plot(num):
     plane_trajectory.set_data(x[0:num],y[0:num])
+
     plane_1.set_data([x[num]-40,x[num]+20],[y[num],y[num]])
     plane_2.set_data([x[num]-20,x[num]],[y[num]+0.3,y[num]])
     plane_3.set_data([x[num]-20,x[num]],[y[num]-0.3,y[num]])
     plane_4.set_data([x[num]-40,x[num]-30],[y[num]+0.15,y[num]])
     plane_5.set_data([x[num]-40,x[num]-30],[y[num]-0.15,y[num]])
 
-    return plane_trajectory,plane_1,plane_2,plane_3,plane_4,plane_5
+    stopwatch0.set_text(str(round(t[num],1))+' hrs')
+    dist_counter0.set_text(str(int(x[num]))+' km')
+
+    return plane_trajectory,plane_1,plane_2,plane_3,plane_4,plane_5,\
+        stopwatch0,dist_counter0
 
 fig=plt.figure(figsize=(16,9),dpi=120,facecolor=(0.8,0.8,0.8))
 gs=gridspec.GridSpec(2,2)
@@ -50,6 +55,15 @@ building_2,=ax0.plot([300,300],[0,1.0],'k',linewidth=7)
 building_3,=ax0.plot([700,700],[0,0.7],'k',linewidth=15)
 building_4,=ax0.plot([900,900],[0,0.9],'k',linewidth=10)
 building_5,=ax0.plot([1300,1300],[0,1.0],'k',linewidth=20)
+
+box_object=dict(boxstyle='square',fc=(0.9,0.9,0.9),ec='g',lw=1)
+stopwatch0=ax0.text(1350,0.85,'',size=20,color='g',bbox=box_object)
+
+box_object2=dict(boxstyle='square',fc=(0.9,0.9,0.9),ec='g',lw=1)
+dist_counter0=ax0.text(1350,0.3,'',size=20,color='r',bbox=box_object2)
+
+
+
 
 plt.xlim(x[0],x[-1])
 plt.ylim(0,y[0]+1)
