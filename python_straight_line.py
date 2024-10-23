7314a90e1fcec448b6c048dc17930db9c93c0016
Mark Peters <mapeters101@earthlink.net>
2024-10-23 12:34:36 -0400

    Lesson027:  Complete lesson.

--- python_straight_line.py
+++ python_straight_line.py
@@ -25,8 +25,9 @@ frame_amount=len(t)
 def update_plot(num):
     plane_trajectory.set_data(x[0:num],y[0:num])
     plane_1.set_data([x[num]-40,x[num]+20],[y[num],y[num]])
+    plane_2.set_data([x[num]-20,x[num]],[y[num]+0.3,y[num]])
 
-    return plane_trajectory,plane_1
+    return plane_trajectory,plane_1,plane_2
 
 fig=plt.figure(figsize=(16,9),dpi=120,facecolor=(0.8,0.8,0.8))
 gs=gridspec.GridSpec(2,2)
@@ -35,6 +36,7 @@ gs=gridspec.GridSpec(2,2)
 ax0=fig.add_subplot(gs[0,:],facecolor=(0.9,0.9,0.9))
 plane_trajectory,=ax0.plot([],[],'g',linewidth=2)
 plane_1,=ax0.plot([],[],'k',linewidth=10)
+plane_2,=ax0.plot([],[],'k',linewidth=5)
 
 # Draw buildings
 building_1,=ax0.plot([100,100],[0,1.0],'k',linewidth=7)
